2bd6089cd317ecbf9edd354ced3ba8f692d6f12c
rscruz <santiagocrzzz1711@gmail.com>
2023-05-14 17:38:56 -0500

    Creando el modelo cliente-servidor

---
+++ OpenMarket-server/src/main/java/co/edu/unicauca/openmarket/openmarket/server/OpenMarketServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.openmarket.openmarket.server;
+
+/**
+ *
+ * @author SANTIAGO
+ */
+public class OpenMarketServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
